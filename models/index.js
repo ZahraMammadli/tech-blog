24e092b9a4d9f7370fbc60ddb8023876ec840bd8
Zahra Mammadli <zahra.mrj@gmail.com>
2022-01-08 12:13:06 +0100

    Work on CSS and styling part

--- models/index.js
+++ models/index.js
@@ -1,32 +1,38 @@
-const User = require("./user");
-const Post = require("./posts");
-const Comment = require("./comments");
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require ('./Comment');
 
+// User has many Posts
 User.hasMany(Post, {
-  foreignKey: "user_id",
-});
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+})
 
-Post.belongsTo(User, {
-  foreignKey: "user_id",
-});
-
-Comment.belongsTo(Post, {
-  foreignKey: "post_id",
-});
-
-Post.hasMany(Comment, {
-  foreignKey: "post_id",
-  // When we delete a Post, we make sure to also delete the associated comments.
-  onDelete: "CASCADE",
-});
-
+// User has many Comments
 User.hasMany(Comment, {
-  foreignKey: "user_id",
-});
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+})
 
+// Post belongs to one User
+Post.belongsTo(User, {
+    foreignKey: "user_id"
+})
+
+// Post has many Comments
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+    onDelete: "CASCADE"
+})
+
+// Comments belong to one User
 Comment.belongsTo(User, {
-  foreignKey: "user_id",
-});
+    foreignKey: "user_id"
+})
 
-// We package our models and export them as an object so we can import them together and use their proper names
-module.exports = { Post, User, Comment };
+// Comments belongs to one Post
+Comment.belongsTo(Post, {
+    foreignKey: "post_id"
+})
+
+module.exports = { User, Post, Comment }
--- server.js
+++ server.js
@@ -1,15 +1,22 @@
+// Dependencies
 const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+const routes = require("./routes");
+const dateHelper = require("./utils/dateHelper");
+
+console.log("DateHelper", dateHelper);
+
+const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
-const helpers = require("./utils/helpers");
-
+// Set up Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers: dateHelper });
 
 const sess = {
   secret: "Super secret secret",
@@ -18,14 +25,16 @@ const sess = {
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize,
+    checkExpirationInterval: 15 * 60 * 1000, // The interval at which to cleanup expired sessions in milliseconds.
+    expiration: 24 * 60 * 60 * 1000, // The maximum age (in milliseconds) of a valid session.
   }),
 };
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
-
+// Set Handlebars as the default template engine.
 app.engine("handlebars", hbs.engine);
+
 app.set("view engine", "handlebars");
 
 app.use(express.json());
@@ -34,6 +43,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
+// Starts the server to begin listening
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => console.log("Now listening on PORT:" + PORT));
 });
