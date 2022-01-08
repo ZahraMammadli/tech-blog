a3e760886b0a4d2895d9b581f5320177e9ca4686
Zahra Mammadli <zahra.mrj@gmail.com>
2022-01-08 11:47:09 +0100

    Update seed part to remove date

--- seeds/index.js
+++ seeds/index.js
@@ -1,28 +1,20 @@
+const seedUsers = require("./user-seeds");
+const seedPosts = require("./post-seeds");
+const seedComments = require("./comment-seeds");
+
 const sequelize = require("../config/connection");
-const { Post, User, Comment } = require("../models/index");
-const seedUsers = require("./users.json");
-const seedPosts = require("./posts.json");
-const seedComments = require("./comments.json");
 
-const createUsers = async (seedUsers) => {
-  return await User.bulkCreate(seedUsers, {
-    individualHooks: true,
-    returning: true,
-  });
-};
-const createPosts = async (seedPosts) => {
-  return await Post.bulkCreate(seedPosts);
-};
-const createComments = async (seedComments) => {
-  return await Comment.bulkCreate(seedComments);
-};
-
-const seed = async () => {
+const seedAll = async () => {
   await sequelize.sync({ force: true });
-  await createUsers(seedUsers);
-  await createPosts(seedPosts);
-  await createComments(seedComments);
-  await sequelize.close();
+  console.log("\n----- DATABASE SYNCED -----\n");
+  await seedUsers();
+  console.log("\n----- USERS SEEDED -----\n");
+  await seedPosts();
+  console.log("\n----- POSTS SEEDED -----\n");
+  await seedComments();
+  console.log("\n----- COMMENTS SEEDED -----\n");
+
+  process.exit(0);
 };
 
-seed();
+seedAll();
